b16edda280a415911112b7ad6899f96b80a84576
= <nicholas.meese@kroger.com>
2019-06-17 12:52:13 -0600

    Nick | configurations

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,29 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+
+    public WelcomeController(@Value("${welcome.message}") String hello) {
+        this.hello = hello;
+    }
+
+    public String hello;
+
     @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+    public String sayHello()
+    {
+        return this.hello;
     }
 }
